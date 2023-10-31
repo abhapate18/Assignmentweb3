b6f9275b44e97d18ccd383bce71b012463c49187
abhapatel18 <apatel573@myseneca.ca>
2023-10-30 22:40:43 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,74 @@
+/********************************************************************************
+* WEB322 – Assignment 03
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecacollege.ca/About/policies/academic-integrity-policy.html
+*
+* Name: Abha Patel Student ID: 114329220 Date: 10-29-2023
+*
+* Published URL: https://lego-a3-jadorotan.cyclic.app
+*
+********************************************************************************/
+
+const legoData = require("./modules/legoSets");
+const express = require('express');
+const path = require('path'); 
+const app = express(); 
+app.use(express.static('public'));
+const HTTP_PORT = process.env.PORT || 8080;
+
+legoData.initialize();
+
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, 'views', 'home.html');
+    res.sendFile(filePath);
+});
+
+app.get('/About', (req, res) => {
+    const filePath = path.join(__dirname, 'views', 'About.html');
+    res.sendFile(filePath);
+});
+
+app.get('/lego/sets', async (req, res) => {
+    try {
+        const theme = req.query.theme;
+
+        if (theme) {
+            const setsByTheme = await legoData.getSetsByTheme(theme);
+            res.send(setsByTheme);
+        } else {
+            const allSets = await legoData.getAllSets();
+            res.send(allSets);
+        }
+    } catch (error) {
+        console.error('Error in /lego/sets:', error);
+        res.status(404).send('Server: Unable to process the request.');
+    }
+});
+
+app.get('/lego/sets/:setNum', async (req, res) => {
+    const setNumParam = req.params.setNum;
+
+    try {
+        const set = await legoData.getSetsByNum(setNumParam);
+
+        if (set) {
+            res.send(set);
+        } else {
+            res.status(404).send('Server: Lego set not found.');
+        }
+    } catch (error) {
+        console.log('Error in getSetsByNum():', error);
+        res.status(404).send('Server: Unable to find requested set(s).');
+    }
+});
+
+app.use((req, res, next) => {
+    res.status(404);
+    const filePath = path.join(__dirname, 'views', '404.html');
+    res.sendFile(filePath);;
+});
+
+app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
